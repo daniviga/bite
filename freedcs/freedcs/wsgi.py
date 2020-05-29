0431285a0d57a8a6bd9bcb02ec532b0970c9fd46
Daniele Viganò <daniele@vigano.me>
2020-05-29 14:27:38 +0200

    Bootstrap Django project

---
+++ freedcs/freedcs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for freedcs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freedcs.settings')
+
+application = get_wsgi_application()
