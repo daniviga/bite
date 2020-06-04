253411c0fdf82866d5c541cdb05ea125fa289963
Daniele Viganò <daniele@vigano.me>
2020-06-05 00:15:31 +0200

    Telemetry is a POST only and filter GET by device (#9)

--- freedcs/telemetry/views.py
+++ freedcs/telemetry/views.py
@@ -1,22 +1,18 @@
+from django.http import Http404
 from rest_framework.viewsets import ModelViewSet
 
 from telemetry.models import Telemetry
 from telemetry.serializers import TelemetrySerializer
+from rest_framework.response import Response
 
 
-class Telemetry(ModelViewSet):
+class TelemetryView(ModelViewSet):
     queryset = Telemetry.objects.all()
     serializer_class = TelemetrySerializer
 
-    # def post(self, request):
-    #     serializer = DeviceSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # def get(self, request):
-    #     devices = Device.objects.all()
-    #     import pdb; pdb.set_trace()
-    #     serializer = DeviceSerializer(devices)
-    #     return Response(serializer.serial)
+    def list(self, request, device=None):
+        queryset = Telemetry.objects.filter(device__serial=device)
+        if not queryset:
+            raise Http404
+        serializer = TelemetrySerializer(queryset, many=True)
+        return Response(serializer.data)
