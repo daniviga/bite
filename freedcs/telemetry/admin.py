8e5a407b280a1a90eaae1d158fda619bde96d287
Daniele Viganò <daniele@vigano.me>
2020-06-15 22:47:55 +0200

    Implement MQTT ingestion and improve dockerization (#13)
    * Add mqtt-to-db command
    
    * Minor fixes
    
    * Ignore production.py on git
    
    * Add a production conf
    
    * Add django container
    
    * Add gunicorn for prod and traefik

--- freedcs/telemetry/admin.py
+++ freedcs/telemetry/admin.py
@@ -4,7 +4,7 @@ from telemetry.models import Telemetry
 
 @admin.register(Telemetry)
 class TelemetryAdmin(admin.ModelAdmin):
-    readonly_fields = ('device', 'time', 'clock', 'payload',)
+    readonly_fields = ('device', 'transport', 'time', 'clock', 'payload',)
     list_display = ('__str__', 'device')
     list_filter = ('time', 'device__serial')
     search_fields = ('device__serial',)
