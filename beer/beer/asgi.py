09cf23554a113187df0f9dbd48685a851008bf2e
Daniele Viganò <daniele@vigano.me>
2020-06-17 20:08:10 +0200

    Project renaming (#14)
    * Renaming
    
    * Make Django migrations sync to DB availability

--- beer/beer/asgi.py
+++ beer/beer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for freedcs project.
+ASGI config for beer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freedcs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beer.settings')
 
 application = get_asgi_application()
