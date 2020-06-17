c44f06676dca349ca634f75c87f1d6b3cc380fe8
Daniele Viganò <daniele@vigano.me>
2020-06-17 23:27:37 +0200

    Renaming, again

--- bite/bite/wsgi.py
+++ bite/bite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for beer project.
+WSGI config for bite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bite.settings')
 
 application = get_wsgi_application()
