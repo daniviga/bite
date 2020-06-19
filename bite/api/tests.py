68e7dc737cd50f64c218fa4067c23bf0328e5bb3
Daniele Viganò <daniele@vigano.me>
2020-06-19 18:35:37 +0200

    Add some tests

--- bite/api/tests.py
+++ bite/api/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from api.models import Device, WhiteList
 
-# Create your tests here.
+
+class ApiTestCase(TestCase):
+    c = Client()
+
+    def setUp(self):
+        WhiteList.objects.create(serial='test1234')
+        Device.objects.create(serial='test1234')
+
+    def test_no_whitelist(self):
+        response = self.c.post('/api/device/subscribe/',
+                               {'serial': 'test12345'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_subscribe_post(self):
+        WhiteList.objects.create(serial='test12345')
+        response = self.c.post('/api/device/subscribe/',
+                               {'serial': 'test12345'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_subscribe_get(self):
+        response = self.c.get('/api/device/list/')
+        self.assertEqual(
+            response.json()[0]['serial'], 'test1234')
