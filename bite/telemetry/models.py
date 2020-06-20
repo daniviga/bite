762605c5c4dea846072427bfaa5d8bd1c61b0cb2
Daniele Viganò <daniele@vigano.me>
2020-06-20 14:41:50 +0200

    Add testing suite (#18)
    * Add some tests
    
    * Add an example of NTP with encryption
    
    * Enable TravisCI
    
    * Run sims via Docker
    
    * Improve simulators stage
    
    * Final fix for travis
    
    * Add README docs [skip ci]

--- bite/telemetry/models.py
+++ bite/telemetry/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 from django.contrib.postgres.fields import JSONField
 
 from api.models import Device
+
+
+def telemetry_validation(value):
+    if not value:
+        raise ValidationError("No telemetry has been sent")
 
 
 class Telemetry(models.Model):
@@ -14,7 +20,7 @@ class Telemetry(models.Model):
     clock = models.IntegerField(
         validators=[MinValueValidator(0)],
         null=True)
-    payload = JSONField()
+    payload = JSONField(validators=[telemetry_validation])
 
     class Meta:
         ordering = ['-time', 'device']
--- bite/telemetry/tests.py
+++ bite/telemetry/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from api.models import Device, WhiteList
 
-# Create your tests here.
+
+class ApiTestCase(TestCase):
+    c = Client()
+
+    payload = {
+        'id': 'sensor.server.domain',
+        'light': 434,
+        'temperature': {
+            'celsius': 27.02149,
+            'raw': 239,
+            'volts': 0.770215
+        }
+    }
+
+    telemetry = {
+        'device': 'test1234',
+        'clock': 1591194712,
+        'payload': json.dumps(payload)
+    }
+
+    def setUp(self):
+        WhiteList.objects.create(serial='test1234')
+        Device.objects.create(serial='test1234')
+
+    def test_no_device(self):
+        fake_telemetry = dict(self.telemetry)  # make a copy of the dict
+        fake_telemetry['device'] = '1234test'
+        response = self.c.post('/telemetry/', fake_telemetry)
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_telemetry(self):
+        fake_telemetry = dict(self.telemetry)  # make a copy of the dict
+        fake_telemetry['payload'] = ''
+        response = self.c.post('/telemetry/', fake_telemetry)
+        self.assertEqual(response.status_code, 400)
+
+    def test_telemetry_post(self):
+        response = self.c.post('/telemetry/', self.telemetry)
+        self.assertEqual(response.status_code, 201)
+
+    def test_telemetry_get(self):
+        response = self.c.post('/telemetry/', self.telemetry)
+        response = self.c.get('/telemetry/test1234/last/')
+        self.assertEqual(
+            response.json()['device'], 'test1234')
+        self.assertEqual(
+            response.json()['transport'], 'http')
+        self.assertJSONEqual(
+            json.dumps(response.json()['payload']), self.payload)
