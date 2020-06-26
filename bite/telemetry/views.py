e85d0bf5bd626680c53c8dc880418e170c32d354
Daniele Viganò <daniele@vigano.me>
2020-06-26 17:49:19 +0200

    Add flake8 testing

--- bite/telemetry/views.py
+++ bite/telemetry/views.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
